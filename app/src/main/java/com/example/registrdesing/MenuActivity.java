f9474a32efdad2c8824a095278be17ec3ad3a1a2
DanaBarybaeva <barybaevadana@gmail.com>
2020-11-15 03:43:23 +0600

    'menu'

---
+++ app/src/main/java/com/example/registrdesing/MenuActivity.java
@@ -0,0 +1,52 @@
+package com.example.registrdesing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class MenuActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btnMenu1, btnMenu2;
+    Button btnMenu3, btnMenu4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        btnMenu1 = findViewById(R.id.btnMenu1);
+        btnMenu2 = findViewById(R.id.btnMenu2);
+        btnMenu3 = findViewById(R.id.btnMenu3);
+        btnMenu4 = findViewById(R.id.btnMenu4);
+        btnMenu1.setOnClickListener(this);
+        btnMenu2.setOnClickListener(this);
+        btnMenu3.setOnClickListener(this);
+        btnMenu4.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btnMenu1){
+            Intent menu1 = new Intent(this,Menu1.class);
+            startActivity(menu1);
+        }
+        else if (view.getId() == R.id.btnMenu2){
+            Intent menu2 = new Intent(this,Menu2.class);
+            startActivity(menu2);
+        }
+        else if (view.getId() == R.id.btnMenu3){
+            Intent menu3 = new Intent(this,Meu3.class);
+            startActivity(menu3);
+        }
+        else{
+            Intent menu4 = new Intent(this,Menu4.class);
+            startActivity(menu4);
+
+        }
+    }
+}
+
+
